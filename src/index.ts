26deb3d4b81f99ac2cffb0b6dc9b4ed0f52f3bf0
James Walker <133775550+lucid3ye@users.noreply.github.com>
2025-07-12 18:52:00 -0400

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/* src/index.ts */
+
+import { API } from 'homebridge'
+import { RoombOMaticPlatform } from './platform.js'
+
+import { PLATFORM_NAME, PLUGIN_NAME } from './settings.js'
+
+export = (api: API) => {
+  api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, RoombOMaticPlatform)
+}
