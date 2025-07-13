156ef614dcd6bf6e9d753db5565b7e08b5098caf
James Walker <133775550+lucid3ye@users.noreply.github.com>
2025-07-13 17:47:42 -0400

    Remove unused code and clean up the index.ts file

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 const platform_js_1 = require("./platform.js");
 const settings_js_1 = require("./settings.js");
-const package_json_1 = __importDefault(require("../package.json"));
 module.exports = (api) => {
     // Log plugin version on startup for clarity
-    // (Helps with debugging and confirming deployment)
     // eslint-disable-next-line no-console
-    console.log(`[${settings_js_1.PLUGIN_NAME}] Loaded — Version ${package_json_1.default.version}`);
+    console.log(`[${settings_js_1.PLUGIN_NAME}] Loaded — Version 1.1.5`);
     api.registerPlatform(settings_js_1.PLUGIN_NAME, settings_js_1.PLATFORM_NAME, platform_js_1.RoombOMaticPlatform);
 };
