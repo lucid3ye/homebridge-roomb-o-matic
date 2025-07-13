1eb8908d802426da53b0375e918035003cdcb8be
James Walker <133775550+lucid3ye@users.noreply.github.com>
2025-07-12 22:37:55 -0400

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,18 +1,18 @@
 /* src/settings.ts */
 
 export interface DeviceConfig {
-  blid: string
-  ip?: string
-  name: string
+  blid: string;
+  ip?: string;
+  name: string;
 }
 
 export interface RoombaPlatformConfig {
-  name: string
-  email?: string
-  password?: string
-  devices: DeviceConfig[]
-  debug?: boolean
+  name: string;
+  email?: string;
+  password?: string;
+  devices: DeviceConfig[];
+  debug?: boolean;
 }
 
-export const PLATFORM_NAME = 'RoombOMatic'
-export const PLUGIN_NAME = 'homebridge-roomb-o-matic'
+export const PLATFORM_NAME = 'Roomb-O-Matic';
+export const PLUGIN_NAME = 'homebridge-roomb-o-matic';
