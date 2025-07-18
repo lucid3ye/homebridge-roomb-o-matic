dd16a79031d4ba45b6597ac7f9409a33a9baaf8a
James Walker <133775550+lucid3ye@users.noreply.github.com>
2025-07-17 22:14:16 -0400

    updated
    accessory issues and json frameworks.

--- dist/settings.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/* src/settings.ts */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PLUGIN_NAME = exports.PLATFORM_NAME = void 0;
-exports.PLATFORM_NAME = 'RoombOMatic';
-exports.PLUGIN_NAME = 'homebridge-roomb-o-matic';
