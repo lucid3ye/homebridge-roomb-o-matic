f767d1e21053a01a0cea15de66616ded2d1c7e75
James Walker <133775550+lucid3ye@users.noreply.github.com>
2025-07-17 22:23:02 -0400

    Fix accessory category to FAN for existing and new accessories

--- src/platform.ts
+++ src/platform.ts
@@ -51,13 +51,13 @@ export class RoombOMaticPlatform implements DynamicPlatformPlugin {
 
       if (existingAccessory) {
         this.log.info(`Restoring existing accessory: ${existingAccessory.displayName}`)
-        existingAccessory.category = this.api.hap.Categories.OTHER;
+        existingAccessory.category = this.api.hap.Categories.FAN;
         new RoombaAccessory(this, existingAccessory, this.log, device, this.config, this.api)
         this.api.updatePlatformAccessories([existingAccessory])
       } else {
         this.log.info(`Adding new accessory: ${device.name}`)
         const accessory = new this.api.platformAccessory(device.name, uuid)
-        accessory.category = this.api.hap.Categories.OTHER;
+        accessory.category = this.api.hap.Categories.FAN;
         new RoombaAccessory(this, accessory, this.log, device, this.config, this.api)
         this.api.registerPlatformAccessories('homebridge-roomb-o-matic', 'RoombOMatic', [accessory])
       }
